04714a438223ddcc2e770274009dbce7126ad90c
Renat <70426913+renat2006@users.noreply.github.com>
2023-07-02 22:18:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+unitInfo = [
+    {
+        unitName: "Length",
+        fromName: "meters",
+        toName: "feet",
+        ratio: 3.281
+    },
+    {
+        unitName: "Volume",
+        fromName: "liters",
+        toName: "gallons",
+        ratio: 0.264
+    },
+    {
+        unitName: "Mass",
+        fromName: "kilograms",
+        toName: "pounds",
+        ratio: 2.204
+    }
+]
+const convertBtn = document.getElementById("convert-btn")
+const numToConvertEl = document.getElementById("num-to-convert")
+const dataBlocks = document.getElementById("data-blocks")
+const render = (data, numToConvert) => {
+    let blockString = ''
+    for (let num in data) {
+        blockString += `
+        <div class="block">
+        <div class="block-heading">
+            ${data[num].unitName} (${data[num].fromName.toUpperCase()}/${data[num].toName.toUpperCase()})
+            
+        </div>
+        <div class="block-data">${numToConvert} ${data[num].fromName} = ${numToConvert * data[num].ratio} 
+${data[num].toName} | ${numToConvert} ${data[num].toName} = ${(numToConvert / data[num].ratio).toFixed(3)} ${data[num].fromName}</div>
+    </div>
+        
+        `
+    }
+    dataBlocks.innerHTML = blockString
+}
+convertBtn.addEventListener("click", () => {
+    const numToConvert = Number(numToConvertEl.value)
+    if (!numToConvert) {
+        dataBlocks.innerHTML = `
+        <div class="block" >
+        <div class="block-heading" style="color: #ff5757;">
+            Error!
+            
+        </div>
+        <div class="block-data">Please enter a valid data</div>
+    </div>
+        
+        `
+    } else {
+        render(unitInfo, numToConvert)
+    }
+
+})
+
